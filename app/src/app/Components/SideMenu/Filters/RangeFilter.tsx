c7563fe989cbcc64e4f39c9cc070e8fd0a10315f
GDDario <gd.dario.dev@gmail.com>
2025-05-11 22:05:52 -0300

    feat(app): integrate range filter with the form

--- app/src/app/Components/SideMenu/Filters/RangeFilter.tsx
+++ app/src/app/Components/SideMenu/Filters/RangeFilter.tsx
@@ -1,33 +1,45 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {Slider} from "@/components/ui/slider";
+import {SliderRange} from "@radix-ui/react-slider";
+import {Controller} from "react-hook-form";
 
 type RangeFilterProps = {
     name: string;
     label: string;
     minimum: number;
     maximum: number;
-    field: any;
+    control: any;
 };
 
-const RangeFilter: React.FC<RangeFilterProps> = ({name, label, maximum, minimum, field}: RangeFilterProps) => {
+const RangeFilter: React.FC<RangeFilterProps> = ({name, label, maximum, minimum, control}: RangeFilterProps) => {
     return (
-        <div>
-            <FormItem>
-                <FormLabel className="text-white">{label}</FormLabel>
-                <FormControl>
-                    <Slider
-                        min={minimum}
-                        max={maximum}
-                    />
-                    {/*<Input placeholder="user@email.com"*/}
-                    {/*       className={success ? "text-green-300 border-green-300" : "text-white"}/>*/}
-                </FormControl>
+        <FormItem>
+            <FormLabel>{label}</FormLabel>
 
-                <FormMessage/>
-            </FormItem>
-        </div>
+            <Controller
+                control={control}
+                name={name}
+                defaultValue={[minimum, maximum]}
+                render={({ field }) => (
+                    <>
+                        <Slider
+                            min={minimum}
+                            max={maximum}
+                            step={10}
+                            value={field.value}
+                            onValueChange={field.onChange}
+                        />
+                        <div className="text-sm text-muted-foreground mt-1 text-right">
+                            R$ {field.value?.[0]} - R$ {field.value?.[1]}
+                        </div>
+                    </>
+                )}
+            />
+
+            <FormMessage />
+        </FormItem>
     );
 };
 
