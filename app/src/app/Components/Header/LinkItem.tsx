7eb33ddf9805d577286b3c5274d2b36c2fd07555
GDDario <gd.dario.dev@gmail.com>
2025-05-26 20:50:37 -0300

    feat(front): mark nav item marked if the current path matches the link location

--- app/src/app/Components/Header/LinkItem.tsx
+++ app/src/app/Components/Header/LinkItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {Button} from "@/components/ui/button";
 
 type LinkItemProps = {
@@ -7,10 +7,24 @@ type LinkItemProps = {
 };
 
 const LinkItem: React.FC<LinkItemProps> = ({location, text}: LinkItemProps) => {
+    const isActive: boolean = useMemo(() => {
+        const href: string = window.location.href;
+        const locations: string[] = href.split("/");
+
+        if (locations.length === 5) {
+            const linkedPath: string = `/${locations[3]}/${locations[4]}`;
+            if (linkedPath === location) {
+                return true;
+            }
+        }
+
+        return false;
+    }, [location]);
+
     return (
         <li>
             <a href={location}>
-                <Button variant="link" className="font-normal text-md">
+                <Button variant="link" className={`font-normal text-md ${isActive && 'underline'}`}>
                     {text}
                 </Button>
             </a>
